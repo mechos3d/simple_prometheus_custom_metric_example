6549ef10bab2ee4ae56ba8133bd111b1aed26cd7
Gorgolevskiy Pavel <gorgolevsky@gmail.com>
2021-09-02 00:33:30 +0300

    some temporary stuff - just a proof-of-concept

--- simple_server/app.rb
+++ simple_server/app.rb
@@ -1,6 +1,25 @@
 # myapp.rb
 require 'sinatra'
+require 'prometheus/client'
+require 'prometheus/middleware/exporter'
 
-get '/foo' do
-  'Hello world!'
+class Application < Sinatra::Base
+  use Rack::Deflater
+  # use Prometheus::Middleware::Collector
+  use Prometheus::Middleware::Exporter
+
+  # returns a default registry
+  prometheus = Prometheus::Client.registry
+  # create a new counter metric
+  foo_custom_counter = Prometheus::Client::Counter.new(:foo_custom_counter, docstring: 'A simple custom counter example')
+  # register the metric
+  prometheus.register(foo_custom_counter)
+
+  puts 'Done initializing'
+
+  get '/foo' do
+    # start using the counter
+    foo_custom_counter.increment
+    'foo1'
+  end
 end
